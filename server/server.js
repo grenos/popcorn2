ab79a74f6fb96415fa9f8bd624d5f9ce404375ed
grenos <Vasilis.green@gmail.com>
2019-05-15 21:14:30 +0200

    add libs

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 1234;
+
+// serve all assets from public folder
+app.use(express.static(publicPath));
+
+// serve always the index to avoid error on refresh
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log('server is running');
+});
