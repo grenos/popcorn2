fb193908775e86b38ccf7c5e6d8ced74e648358d
mcsaatchidevelopers <tools@mcsaatchi-milano.com>
2019-05-16 18:35:04 +0200

    up

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import logo from '../../media/img/logo.png'
+import { Waypoint } from 'react-waypoint'
+import { useSpring, animated, config } from 'react-spring'
 
 
 const Nav = () => {
+
+  const [scroll, useScroll] = useState();
+
   return (
-    <div>
-      <h1>test</h1>
+    <div className="nav">
+      <img src={logo} alt="logo" className="nav__img" />
     </div>
   )
 }
-
 
 
 
