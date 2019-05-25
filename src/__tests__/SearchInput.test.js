ef3e67acf27583ad0bad596407fcae5a4d2fbd52
grenos <Vasilis.green@gmail.com>
2019-05-25 10:32:11 +0200

    up

--- src/__tests__/SearchInput.test.js
+++ src/__tests__/SearchInput.test.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import Enzyme, { shallow } from 'enzyme'
+import Enzyme, { shallow, mount } from 'enzyme'
+import { act } from 'react-dom/test-utils';
 import EnzymeAdapter from 'enzyme-adapter-react-16'
 import SearchInput, { UnconnectedSearchInput } from '../components/SearchInput/SearchInput'
 import { findByTestAttr, storeFactory } from '../helpers/testUtils'
@@ -34,6 +35,10 @@ describe('should render components', () => {
 
 
 
+
+
+
+
 describe('guessWord action creator call', () => {
   let getUserInputRequestMock
   let wrapper
@@ -42,17 +47,29 @@ describe('guessWord action creator call', () => {
   beforeEach(() => {
     getUserInputRequestMock = jest.fn()
     const props = {
-      getUserInputRequest: getUserInputRequestMock
+      handleChange: getUserInputRequestMock
     }
 
     wrapper = shallow(<UnconnectedSearchInput {...props} />).dive()
 
-    const component = findByTestAttr(wrapper, 'search-input')
-    const input = component.dive()
+    const input = findByTestAttr(wrapper, 'search-input').dive()
+    // const component = findByTestAttr(wrapper, 'search-input').last()
+    // console.log(input.debug());
     expect(input.name()).toBe('input')
 
-    // input.simulate({ currentTarget: { value: userInput } })
-    // expect(input.dive().prop('value')).toBe(userInput);
+    expect(getUserInputRequestMock).not.toHaveBeenCalled()
+
+    // input.simulate('change', { target: { value: userInput } })
+    input.props().onChange({ target: { value: userInput } })
+
+
+    // act(() => {
+    //   input.props().onChange({ currentTarget: { value: userInput } })
+    // })
+
+    // wrapper.update()
+    expect(getUserInputRequestMock).toBeCalledTimes(1)
+    // expect(input.prop('value')).toBe(userInput);
 
   })
 
