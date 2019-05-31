aa21b1682dbf8b130a293f48500f370a7bf3e6f3
mcsaatchidevelopers <tools@mcsaatchi-milano.com>
2019-05-31 17:54:56 +0200

    up

--- src/__tests__/MenuToggle.test.js
+++ src/__tests__/MenuToggle.test.js
@@ -2,10 +2,52 @@ import React from 'react'
 import Enzyme, { shallow } from 'enzyme'
 import EnzymeAdapter from 'enzyme-adapter-react-16'
 import { findByTestAttr, storeFactory } from '../helpers/testUtils'
-import MenuToggle, {UnconnectedMenuToggle} from '../components/MenuToggle/MenuToggle'
+import MenuToggle, { UnconnectedMenuToggle } from '../components/MenuToggle/MenuToggle'
 Enzyme.configure({ adapter: new EnzymeAdapter() })
 
 
-test('should ', () => {
-  
+const setup = (initialState = {}) => {
+  const store = storeFactory(initialState)
+  const wrapper = shallow(<MenuToggle store={store} />).dive()
+  return wrapper
+}
+
+test('should render button component', () => {
+  const wrapper = setup()
+  const button = findByTestAttr(wrapper, 'button-toggle')
+  expect(button.length).toBe(1)
 })
+
+
+test('should animate button on click', () => {
+  const defaultProps = { transform: "translate3d(0px,0,0)" }
+  const wrapper = setup().dive()
+  const button = findByTestAttr(wrapper, 'button-toggle')
+
+  const buttonNotClicked = button.prop('style')
+  expect(buttonNotClicked).toEqual(defaultProps)
+
+  button.simulate('click')
+  console.log(findByTestAttr(wrapper, 'button-toggle').prop('style'));
+
+  // const buttonClicked = findByTestAttr(wrapper, 'button-toggle').prop('style')
+
+  // expect(findByTestAttr(wrapper, 'button-toggle').prop('style')).toEqual(0)
+})
+
+
+// test('should animate img on click', () => {
+//   const wrapper = setup().dive()
+//   const button = findByTestAttr(wrapper, 'button-toggle')
+//   const img = findByTestAttr(wrapper, 'img-toggle')
+
+//   const imgNotClicked = button.style
+//   expect(imgNotClicked).toEqual(img.props.style)
+
+//   button.simulate('click')
+//   const imgClicked = findByTestAttr(wrapper, 'button-toggle')
+//   const toggleCSSprops = imgClicked.props.style
+
+//   expect(toggleCSSprops).toEqual(imgClicked.props.style)
+// })
+
--- src/components/MenuToggle/MenuToggle.tsx
+++ src/components/MenuToggle/MenuToggle.tsx
@@ -7,7 +7,7 @@ import chevron from '../../media/img/chevron.png'
 
 export const UnconnectedMenuToggle = ({ getToggleMenuRequest }: any) => {
 
-  const [isMenuOpen, setNavOpen] = useState(false);
+  const [isMenuOpen, setMenuOpen] = useState(false);
 
   const btnAnimation = useSpring({
     transform: isMenuOpen
@@ -18,30 +18,30 @@ export const UnconnectedMenuToggle = ({ getToggleMenuRequest }: any) => {
   const imgAnimation = useSpring({
     transform: isMenuOpen
       ? `rotate(0deg)`
-      : `rotate(180deg)`
+      : `rotate(1980deg)`
   });
 
   const makeBoolGlobal = () => {
-    setNavOpen(isMenuOpen => !isMenuOpen)
-    // Hack!?!? maybe not
+    setMenuOpen(isMenuOpen => !isMenuOpen)
+    // Hack!?!? maybe 
     getToggleMenuRequest(!isMenuOpen)
   }
 
   return (
     <a.button
+      data-test="button-toggle"
       className="menu-button"
       onClick={makeBoolGlobal}
       style={btnAnimation}
     >
       <a.img src={chevron}
+        data-test="img-toggle"
         alt="Open Menu"
         style={imgAnimation}
       />
     </a.button>
   )
 }
-
-
 
 export default connect(null, {
   getToggleMenuRequest,
