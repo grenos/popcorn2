d9d275fd2f95cbeb9b8243f72f7d0c43d81e9075
mcsaatchidevelopers <tools@mcsaatchi-milano.com>
2019-06-07 16:35:19 +0200

    up

--- src/redux/state/uiState.ts
+++ src/redux/state/uiState.ts
@@ -3,7 +3,7 @@ import * as INT from '../../helpers/interfaces'
 
 export const UI_STATE: INT.IUiState = {
   isMenuOpen: false,
-  isMovieCatSelected: false,
+  isMovieCatSelected: true,
   isSerieCatSelected: false
 }
 
