2924a05bb170e3961b35d7fbc6857852db8be687
grenos <Vasilis.green@gmail.com>
2019-06-08 23:25:41 +0200

    up

--- src/components/TopItems/TopItems.tsx
+++ src/components/TopItems/TopItems.tsx
@@ -1,6 +1,8 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 import * as INT from '../../helpers/interfaces'
+import { Trail } from 'react-spring/renderprops.cjs';
+import popcorn from '../../media/img/popcorn.png'
 
 const URL = 'https://image.tmdb.org/t/p/w500/'
 
@@ -10,40 +12,57 @@ const TopItems: React.FC<INT.ITopResultsProps> = ({
   topSeries,
 }): JSX.Element => {
 
-  useEffect(() => {
-    console.log(topMovies);
-  })
-
   const renderMovies = () => {
     return (
-      topMovies.map(({ id, title, poster_path, vote_average }) => {
-        return (
-          <div key={id} className="locandina-outer">
+      <Trail
+        items={topMovies}
+        keys={topMovies => topMovies.id}
+        config={{ tension: 215, mass: .4, friction: 14 }}
+        from={{ opacity: 0, transform: 'scale(0.8)' }}
+        to={{ opacity: 1, transform: 'scale(1)' }}
+      >
+        {({ id, title, poster_path, vote_average }) => ({ opacity, transform }) =>
+          <div key={id}
+            className="locandina-outer"
+            style={{ opacity, transform }}>
             <img src={URL + poster_path} alt="img" />
             <div className="overlay-gallery">
               <h3 style={{ color: 'white' }}>{title}</h3>
-              <p>{vote_average}</p>
+              <div className="ratings">
+                <p>{vote_average}</p>
+                <img src={popcorn} alt="logo" />
+              </div>
             </div>
           </div>
-        )
-      })
+        }
+      </Trail>
     )
   }
 
-
   const renderSeries = () => {
     return (
-      topSeries.map(({ id, name, poster_path, vote_average }) => {
-        return (
-          <div key={id} className="locandina-outer">
+      <Trail
+        items={topSeries}
+        keys={topSeries => topSeries.id}
+        config={{ tension: 215, mass: .4, friction: 14 }}
+        from={{ opacity: 0, transform: 'scale(0.8)' }}
+        to={{ opacity: 1, transform: 'scale(1)' }}
+      >
+        {({ id, name, poster_path, vote_average }) => ({ opacity, transform }) =>
+          <div key={id}
+            className="locandina-outer"
+            style={{ opacity, transform }}>
             <img src={URL + poster_path} alt="img" />
             <div className="overlay-gallery">
-              <h3 style={{ color: 'white' }}>{name}</h3>
-              <p>{vote_average}</p>
+              <h3>{name}</h3>
+              <div className="ratings">
+                <p>{vote_average}</p>
+                <img src={popcorn} alt="logo" />
+              </div>
             </div>
           </div>
-        )
-      })
+        }
+      </Trail>
     )
   }
 
